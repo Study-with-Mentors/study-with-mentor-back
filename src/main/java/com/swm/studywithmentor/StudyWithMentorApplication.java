de74ec9bff77524cd4880757826210b75182a207
Tran Dinh Khanh <54855318+khanhdinh02@users.noreply.github.com>
2023-05-17 08:32:28 +0700

    init

---
+++ src/main/java/com/swm/studywithmentor/StudyWithMentorApplication.java
@@ -0,0 +1,13 @@
+package com.swm.studywithmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithMentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWithMentorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swm/studywithmentor/StudyWithMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.studywithmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWithMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
