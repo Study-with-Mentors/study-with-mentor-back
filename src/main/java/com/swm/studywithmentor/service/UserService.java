ff50b36fe4c9eafe4a1b58f73b7f93b9212cf361
Duy Nguyen <nguyenduduy233@gmail.com>
2023-06-11 13:02:57 +0700

    Add get current user to `UserService`

--- src/main/java/com/swm/studywithmentor/service/UserService.java
+++ src/main/java/com/swm/studywithmentor/service/UserService.java
@@ -1,6 +1,8 @@
 package com.swm.studywithmentor.service;
 
+import com.swm.studywithmentor.model.entity.user.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
+    User getCurrentUser();
 }
--- src/main/java/com/swm/studywithmentor/service/impl/UserServiceImpl.java
+++ src/main/java/com/swm/studywithmentor/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.swm.studywithmentor.model.entity.user.User;
 import com.swm.studywithmentor.repository.UserRepository;
 import com.swm.studywithmentor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -31,5 +33,11 @@ public class UserServiceImpl implements UserService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
             .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+
+    @Override
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
     }
 }
